507aa7343ecd49f9b8bccee92148f56a6f41cb9b
Joao Pedro Barbosa <jaopedrobarbosa15@gmail.com>
2023-10-03 15:38:44 -0400

    payment

--- src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -3,31 +3,27 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
-
 @Entity
-@Table(name="tb_order")
-public class Order {
+@Table(name="tb_payment")
+public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
+    @OneToOne
+    @MapsId
+    private Order order;
 
-    public Order(){
+
+    public Payment(){
 
     }
 
-    public Order(long id, Instant moment, OrderStatus status) {
+    public Payment(long id, Instant moment) {
         this.id = id;
         this.moment = moment;
-        this.status = status;
     }
 
     public long getId() {
@@ -36,9 +32,5 @@ public class Order {
 
     public Instant getMoment() {
         return moment;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
     }
 }
